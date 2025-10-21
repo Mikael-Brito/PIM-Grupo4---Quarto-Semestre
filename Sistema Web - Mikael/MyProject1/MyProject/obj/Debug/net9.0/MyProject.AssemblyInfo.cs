0cc3a719b8fcd3312bbcf0e753fb5a806bb1ef4d
Mikael-Brito <mikaelbrito40@gmail.com>
2025-10-20 21:48:01 -0300

    Criação Abrir chamado

--- Sistema Web - Mikael/MyProject1/MyProject/obj/Debug/net9.0/MyProject.AssemblyInfo.cs
+++ Sistema Web - Mikael/MyProject1/MyProject/obj/Debug/net9.0/MyProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e7dbd10087eb73e6ca03cd57131fa9bbe5207aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
