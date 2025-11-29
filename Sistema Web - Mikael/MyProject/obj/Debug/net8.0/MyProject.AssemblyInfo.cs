2927259a0b7bce5d5af6d19063f176c0eac55336
Mikael-Brito <mikaelbrito40@gmail.com>
2025-11-29 19:20:25 -0300

    Helpdesk - ads
    Atualização de API para IA "Ollama"

--- Sistema Web - Mikael/MyProject/obj/Debug/net8.0/MyProject.AssemblyInfo.cs
+++ Sistema Web - Mikael/MyProject/obj/Debug/net8.0/MyProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53b02d9c7850b131e5887d4f3c2899ab21df2e23")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd51443894457e69512d90262f5f07d5c397c376")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
